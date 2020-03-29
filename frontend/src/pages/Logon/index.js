0c73f88820fb12f24b8740b52523d8649398deae
Treinamento <Treinamento@RD723942.raiadrogasil.com>
2020-03-29 02:05:16 -0300

    Finalizada a parte de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,16 +10,40 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(evt) {
+        evt.preventDefault();
+
+        try {
+           const response = await api.post('sessions', { id });
+
+           localStorage.setItem('ongId', id);
+           localStorage.setItem('ongName', response.data.name);
+
+           history.push('/profile');
+        } catch (error) {
+            alert('Falha no login, tente novamente')
+            
+        }
+    }
+
     return (
         <div>
             <div className="logon-container">
                 <section className="form">
                     <img src={logoImg} alt="Be The Hero" />
 
-                    <form>
+                    <form onSubmit={handleLogin}>
                         <h1>Faça seu logon</h1>
 
-                        <input placeholder="Sua ID" />
+                        <input 
+                            placeholder="Sua ID" 
+                            value={id}
+                            onChange={evt => setId(evt.target.value)}
+                        />
                         <button className="button" type="submit">Entrar</button>
 
                         <Link className="back-link" to="/register">
